b86e6b4b0b1e2c2a177c652de827a8c7f58bceb3
faizanhussain0 <faizan0hussain@gmail.com>
2025-12-20 02:11:45 +0530

    Update __init__.py

--- plant_disease_classification/__init__.py
+++ plant_disease_classification/__init__.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
-
-'''Disease classification on different plants with Neural Networks.'''
-
 from __future__ import absolute_import
-
-__author__       = 'Abdullah Selek'
-__email__        = 'abdullahselek.os@gmail.com'
-__copyright__    = 'Copyright (c) 2018 Abdullah Selek'
-__license__      = 'MIT License'
-__version__      = '0.1'
-__url__          = 'https://github.com/abdullahselek/plant-disease-classification'
-__download_url__ = 'https://github.com/abdullahselek/plant-disease-classification'
-__description__  = 'Disease classification on different plants with Neural Networks.'
 
 from plant_disease_classification import (
     datagenerator,
@@ -19,3 +7,4 @@ from plant_disease_classification import (
 )
 
 from plant_disease_classification.dataset import DataSet
+
